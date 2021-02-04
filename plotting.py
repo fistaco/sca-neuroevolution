f0270b003ca2b4ca31121c12735e02b2734f4a3b
fistaco <fiske_schijlen@hotmail.com>
2021-02-04 15:43:59 +0100

    Implement plotting for an SCA with varying amounts of atk traces.

--- plotting.py
+++ plotting.py
@@ -19,3 +19,23 @@ def plot_gens_vs_fitness(fitnesses_per_gen, experiment_name):
     plt.grid(True)
     plt.savefig(experiment_name)
     plt.clf()
+
+
+def plot_n_traces_vs_key_rank(rank_per_trace_num, experiment_name):
+    """
+    Constructs and saves a plot with the amount of traces on the x-axis and the
+    (mean) key rank on the y-axis.
+
+    Arguments:
+        rank_per_trace_num: A dictionary with the trace amounts as keys and the
+        corresponding (mean) key ranks as values.
+
+        experiment_name: The name of the results' corresponding experiment.
+    """
+    plt.title(f"Amount of traces ~ key rank ({experiment_name})")
+    plt.xlabel("Traces")
+    plx.ylabel("Key rank")
+    plt.plot(rank_per_trace_num.keys(), rank_per_trace_num.values())
+    plt.grid(True)
+    plt.savefig(experiment_name)
+    plt.clf()
