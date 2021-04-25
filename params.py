6c5d22ddf0bb4dfd90a5fa52bb7b01baba3a729b
fistaco <fiske_schijlen@hotmail.com>
2021-04-25 16:52:06 +0200

    Use more conservative default parameters.

--- params.py
+++ params.py
@@ -1,10 +1,14 @@
+from metrics import MetricType
+
+
 # Genetic algorithm parameters
 MUTATION_POWER = 0.03
 MUTATION_RATE = 0.04
 CROSSOVER_RATE = 0.5
-MAX_GENERATIONS = 100
-POPULATION_SIZE = 1000
+MAX_GENERATIONS = 50
+POPULATION_SIZE = 50
 TRUNCATION_PROPORTION = 0.4
+SELECTION_FUNCTION = "tournament"
 TOURNAMENT_SIZE = 3
 
 # Params specific to the approach from the paper by Morse and Stanley (2016)
@@ -12,4 +16,5 @@ MUTATION_POWER_DECAY = 0.99
 FITNESS_INHERITANCE_DECAY = 0.2
 
 # SCA-specific parameters
-ATTACK_SET_SIZE = 1000  # Size of the set used for fitness evaluation of an NN
+ATTACK_SET_SIZE = 2048  # Size of the set used for fitness evaluation of an NN
+METRIC_TYPE = MetricType.INCREMENTAL_KEYRANK
