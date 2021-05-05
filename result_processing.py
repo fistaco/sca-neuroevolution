626b55ddcf95a2f8fd90f5e861fa78070d72f821
fistaco <fiske_schijlen@hotmail.com>
2021-05-05 12:31:27 +0200

    Implement result DF processing for new grid search.

--- result_processing.py
+++ result_processing.py
@@ -3,7 +3,7 @@ from enum import Enum
 import sys
 
 import numpy as np
-from helpers import gen_extended_exp_name, gen_ga_grid_search_arg_lists
+from helpers import gen_extended_exp_name, gen_mini_grid_search_arg_lists
 from nn_genome import NeuralNetworkGenome
 
 
@@ -12,77 +12,65 @@ def combine_grid_search_results():
     Loads the experimental results from experiments that were run for a grid
     search using the averaged_ga_experiment method.
     """
-    argss = gen_ga_grid_search_arg_lists()
+    argss = gen_mini_grid_search_arg_lists()
     n_exps = len(argss)
 
-    key_rank_zero_indivs = []
-
-    best_key_rank = 255
+    best_inc_kr = 3.0
     best_run_results = None
-    best_avg_keyrank = 255
+    best_avg_inc_kr = 3.0
     best_experiment_data = None
+    n_repeats = 5
 
-    df = np.zeros((n_exps*10, len(ResultCategory)), dtype=object)
+    df = np.zeros((n_exps*n_repeats, len(ResultCategory)), dtype=object)
     run_nr = 0
-    for (exp_idx, (ps, mp, mr, mpdr, fdr, ass, sm, mt)) in enumerate(argss):
-        sys.stdout.write(f"\rProcessing results from experiment {exp_idx}/{n_exps - 1}")
-        sys.stdout.flush()
+    for (exp_idx, args) in enumerate(argss):
+        args = (ps, mp, mr, mpdr, fdr, ass, sf, mt, n_folds, fi, bt, tp, cor)
+        print(f"Processing results from experiment {exp_idx}/{n_exps - 1}")
 
-        name = gen_extended_exp_name(ps, mp, mr, mpdr, fdr, ass, sm, mt, "mlp")
+        name = gen_extended_exp_name(*args)
         dir_path = f"grid-search-results/{name}"
 
-        avg_key_rank = 0
-        for i in range(10):
+        avg_inc_kr = 0
+        for i in range(n_repeats):
             # Load results
             filepath = f"{dir_path}/run{i}_results.pickle"
             results = None
             with open(filepath, "rb") as f:
                 results = pickle.load(f)
-            (best_indiv, _, _, key_rank) = results
-            fitness = best_indiv.fitness
+            (best_indiv, best_fitness_per_gen, top_ten, fit, inc_kr) = results
 
-            avg_key_rank += key_rank/10
+            avg_inc_kr += inc_kr/n_repeats
 
             # # Update best results so far
-            if key_rank < best_key_rank:
-                best_key_rank = key_rank
+            if inc_kr < best_inc_kr:
+                best_inc_kr = inc_kr
                 best_run_results = results
 
-                print(f"=== New best key rank: {key_rank} ===")
+                print(f"=== New best INC_KR: {inc_kr} ===")
                 print(f"=== Achieved with: {name} ===")
-            
-            # Store the potential best NNs
-            if key_rank == 0:
-                key_rank_zero_indivs.append((best_indiv, name))
 
             # Put results in dataframe
-            df[run_nr] = \
-                [ps, mp, mr, mpdr, fdr, ass, sm, mt, "mlp", key_rank, fitness]
+            df[run_nr] = [mp, mr, mpdr, sf, tp, cor, fit, inc_kr]
 
             run_nr += 1
 
         # Update best results so far
-        if avg_key_rank < best_avg_keyrank:
-            best_avg_keyrank = avg_key_rank
-            best_experiment_data = \
-                (ps, mp, mr, mpdr, fdr, ass, sm, mt, "mlp", exp_idx, key_rank)
+        if avg_inc_kr < best_avg_inc_kr:
+            best_avg_inc_kr = avg_inc_kr
+            best_experiment_data = (mp, mr, mpdr, sf, tp, cor, exp_idx, inc_kr)
 
-            print(f"=== New best avg key rank: {avg_key_rank} ===")
+            print(f"=== New best avg key rank: {avg_inc_kr} ===")
             print(f"=== Achieved with: {name} ===")
-    
-    cols = [ResultCategory.KEY_RANK.value, ResultCategory.FITNESS.value]
-    corr_mat = np.corrcoef(df[:, cols].astype(np.float64).T)
-    print(f"Correlation between key rank and fitness: {corr_mat[0, 1]}")
-    print(f"\nAmount of NNs with key rank 0: {len(key_rank_zero_indivs)}")
 
-    with open("ga_weight_evo_grid_search_results.pickle", "wb") as f:
+    corr_mat = np.corrcoef(df[:, [-1, -2]].astype(np.float64).T)
+    print(f"Correlation between final INC_KR and fitness: {corr_mat[0, 1]}")
+
+    with open("res/static_gs_weight_evo_results_df.pickle", "wb") as f:
         pickle.dump(df, f)
-    with open("ga_weight_evo_grid_search_best_run_results.pickle", "wb") as f:
+    with open("res/static_gs_weight_evo_best_run_results.pickle", "wb") as f:
         pickle.dump(best_run_results, f)
-    with open("ga_weight_evo_grid_best_experiment_data.pickle", "wb") as f:
+    with open("res/static_gs_weight_evo_best_exp_data.pickle", "wb") as f:
         pickle.dump(best_experiment_data, f)
-    with open("ga_weight_evo_grid_key_rank_zero_indivs.pickle", "wb") as f:
-        pickle.dump(key_rank_zero_indivs, f)
 
     print("Finished processing and saving grid search results.")
 
